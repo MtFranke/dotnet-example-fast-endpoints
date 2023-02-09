7bac40bccc7dc2940b9dddf2837aa05c110fb33b
mateuszfranke <mateuszfranke@icloud.com>
2023-02-09 12:22:53 +0100

    remove not used project

--- dotnet-example-fast-endpoints/DTO/MyResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace dotnet_example_fast_endpoints.DTO;
-
-public class MyResponse
-{
-    public string FullName { get; set; }
-    public bool IsOver18 { get; set; }
-}
--- dotnet-example-fast-endpoints/Endpoints/MyEndpoint.cs
+++
@@ -1,9 +0,0 @@
-using dotnet_example_fast_endpoints.DTO;
-using FastEndpoints;
-
-namespace dotnet_example_fast_endpoints.Endpoints;
-
-public class MyEndpoint: Endpoint<MyRequest>
-{
-    
-}
